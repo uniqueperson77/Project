596f56fe3954d2cc921bd81eb315fd8d019c5c53
UniquePerson77 <uniqueperson77@outlook.com>
2025-04-27 12:17:35 +0530

    ---

--- test_testcases/sample-dev2.py
+++ test_testcases/sample-dev2.py
@@ -1,7 +1,7 @@
 def fn1():
     print("dev2dev2")
-    obj = open(r"D:\Practice\Files\file.txt","w")
-    obj.write("HI Master---.")
+    obj = open(r"D:\Practice\Files\file1.txt","w")
+    obj.write("HI Master-----")
     obj.close()
     
 fn1()
